415d3aa4cbcb8553344abd716ca0fb71a7b31a4d
Nietaktowny <87383673+Nietaktowny@users.noreply.github.com>
2023-09-26 19:19:51 +0200

    Added receive and send functions.

--- include/tcp_controller.h
+++ include/tcp_controller.h
@@ -1,9 +1,24 @@
 #pragma once
+#include "freertos/FreeRTOS.h"
+#include "freertos/event_groups.h"
 
 #define TCP_C_PORT                        27015
 #define TCP_C_KEEPALIVE_IDLE              10              ///< TCP keep-alive idle time(s).
 #define TCP_C_KEEPALIVE_INTERVAL          10              ///< TCP keep-alive interval time(s).
 #define TCP_C_KEEPALIVE_COUNT             5               ///< TCP keep-alive packet retry send counts.
-#define TCP_C_TRANSMIT_BUFLEN             128
+#define TCP_C_KEEPALIVE                   1
+#define TCP_C_RECEIVE_BUFLEN              512
+#define TCP_C_SEND_BUFLEN                 512
+#define TCP_C_ADDR_FAMILY                 AF_INET
+#define TCP_C_IP_PROTOCOL                 IPPROTO_IP
 
-void tcp_c_start_tcp_server(void);
+#define TCP_C_RECEIVED_DATA_BIT           0x00000001
+#define TCP_C_SENDED_DATA_BIT             0x00000002
+#define TCP_C_ACCEPTED_SOCKET_BIT         0x00000004
+#define TCP_C_FINISHED_TRANSMISSION       0x00000008
+#define TCP_C_DATA_READY_TO_SEND          0x00000010
+
+int tcp_c_start_tcp_server(EventGroupHandle_t event_group);
+void tcp_c_server_loop(void);
+int tcp_c_receive(char rx_buffer[]);
+int tcp_c_send(const char tx_buffer[], int buflen);
--- src/tcp_controller.c
+++ src/tcp_controller.c
@@ -9,162 +9,186 @@
 #include <lwip/netdb.h>
 #include "freertos/task.h"
 #include "inttypes.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/event_groups.h"
 
 
 static const char* TAG = "tcp_controller";
 
-static int tcp_c_handle_exchange(const int sock);
-static void cli_tcp_server_task(void *pvParameters);
 
-static int tcp_c_receive (void) {
+static void tcp_c_prepare_sock_addr(struct sockaddr_storage *dest_addr);
+static int tcp_c_prepare_listen_socket(void);
+static int tcp_c_bind_socket(int socket, struct sockaddr_storage *dest_addr);
+static int tcp_c_listen_on_socket(int socket);
+static int tcp_c_accept_socket(int socket);
+
+static EventGroupHandle_t tcp_event_group;
+
+static int listen_socket = 0;
+static int connected_socket = 0;
+
+int tcp_c_send(const char tx_buffer[], int buflen) {
     volatile err_c_t err = ERR_C_OK;
-    Try {
-
-    } Catch(err) {
-
+    int to_write = buflen;
+    while (to_write > 0) {
+        int written = send(connected_socket, tx_buffer+(buflen-to_write), to_write, 0);
+        if (written < 0) {
+            ESP_LOGE(TAG, "Error occurred during sending: errno %d", errno);
+            return errno;
+        }
+        to_write -= written;
     }
-
+    xEventGroupSetBits(tcp_event_group, TCP_C_SENDED_DATA_BIT);
     return err;
 }
 
-static int tcp_c_handle_exchange(const int sock)
-{
+int tcp_c_receive(char rx_buffer[]) {
+    volatile err_c_t err = ERR_C_OK;
     int len;
-    char rx_buffer[512];
-    volatile bool status = false;
+    memset(rx_buffer, 0, TCP_C_RECEIVE_BUFLEN);
+    Try {
+        do {
+            len = recv(connected_socket, rx_buffer, TCP_C_RECEIVE_BUFLEN-1, 0);
+            if(len < 0) {
+                ERR_C_SET_AND_THROW_ERR(err, errno);
+            } else if (len == 0) {
+                ESP_LOGD(TAG, "Connection closed.");
+            } else {
+                rx_buffer[len] = 0; // Null-terminate whatever is received and treat it like a string
+                ESP_LOGD(TAG, "Received %d bytes.", len);
+                ESP_LOGD(TAG, "%s", rx_buffer);
+            }
+        } while (len > 0);
+
+    } Catch(err) {
+        ESP_LOGE(TAG, "Error during receiving data, errno: %d", errno);
+        err = errno;
+    }
+    xEventGroupSetBits(tcp_event_group, TCP_C_RECEIVED_DATA_BIT);
+    return err;
+}
+
+static void tcp_c_prepare_sock_addr(struct sockaddr_storage *dest_addr) {
+    struct sockaddr_in *dest_addr_ip4 = (struct sockaddr_in *)dest_addr;
+    dest_addr_ip4->sin_addr.s_addr = htonl(INADDR_ANY);
+    dest_addr_ip4->sin_family = AF_INET;
+    dest_addr_ip4->sin_port = htons(TCP_C_PORT);
+}
+
+static int tcp_c_prepare_listen_socket(void) {
+    int listen_sock = socket(TCP_C_ADDR_FAMILY, SOCK_STREAM, TCP_C_IP_PROTOCOL);
     volatile err_c_t err = ERR_C_OK;
     Try {
-            do {
-                len = recv(sock, rx_buffer, sizeof(rx_buffer) - 1, 0);
-                if (len < 0) {
-                    ERR_C_SET_AND_THROW_ERR(err, errno);
-                } else if (len == 0) {
-                    ESP_LOGD(TAG, "Connection closed");
-                } else {
-                    rx_buffer[len] = 0; // Null-terminate whatever is received and treat it like a string
-                    ESP_LOGD(TAG, "Received %d bytes.", len);
-                    ESP_LOGI(TAG, "%s", rx_buffer);
+        if (listen_sock < 0) {
+            ERR_C_SET_AND_THROW_ERR(err, errno);
+        }
+        int opt = 1;
+        setsockopt(listen_sock, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
 
-                    if(strncmp(&rx_buffer[0], "scan", strlen(rx_buffer)) == 0) {
-                        status = true;
-                    }
-
-                    // send() can return less bytes than supplied length.
-                    // Walk-around for robust implementation.
-                    int to_write = len;
-                    while (to_write > 0) {
-
-                        if(status) {
-                            wifi_c_scan_result_t scan_result;
-                            char temp_buf[312];
-                            wifi_c_scan_all_ap(&scan_result);
-
-                            memset(rx_buffer, 0, sizeof(rx_buffer));
-                            memset(temp_buf, 0, sizeof(temp_buf));
-
-                            strcat(&rx_buffer[0], "Scanned APs:");
-                            wifi_c_store_scanned_ap(temp_buf, 312);
-
-                            strncat(&rx_buffer[12], &temp_buf[0], strlen(temp_buf));
-
-                            len = strlen(rx_buffer);
-                            to_write = len;
-                        }
-
-                        int written = send(sock, rx_buffer + (len - to_write), to_write, 0);
-                        if (written < 0) {
-                            ERR_C_SET_AND_THROW_ERR(err, errno);
-                            ESP_LOGE(TAG, "Error occurred during sending: errno %d", errno);
-                            // Failed to retransmit, giving up
-                            return errno;
-                        }
-                        to_write -= written;
-                    }
-                }
-        } while (len > 0);
     } Catch(err) {
-        ESP_LOGE(TAG, "Error occurred during receiving: errno %d", errno);
+        ESP_LOGE(TAG, "Unable to create socket: errno %d", err);  
+        Throw(err);
+    }
+
+    return listen_sock;
+}
+
+static int tcp_c_bind_socket(int socket, struct sockaddr_storage *dest_addr) {
+    volatile err_c_t err = ERR_C_OK;
+    Try {
+        err = bind(socket, (struct sockaddr *)dest_addr, sizeof(struct sockaddr_storage));
+        ERR_C_CHECK_AND_THROW_ERR(err);
+    } Catch(err) {
+        ESP_LOGE(TAG, "Socket unable to bind: errno %d", errno);
     }
 
     return err;
 }
 
-static void cli_tcp_server_task(void *pvParameters)
-{
-    err_c_t err = ERR_C_OK;
-    char addr_str[128];
-    int addr_family = AF_INET;
-    int ip_protocol = 0;
-    int keepAlive = 1;
-    int keepIdle = TCP_C_KEEPALIVE_IDLE;
-    int keepInterval = TCP_C_KEEPALIVE_INTERVAL;
-    int keepCount = TCP_C_KEEPALIVE_COUNT;
-    struct sockaddr_storage dest_addr;
-
-
-    if (addr_family == AF_INET) {
-        struct sockaddr_in *dest_addr_ip4 = (struct sockaddr_in *)&dest_addr;
-        dest_addr_ip4->sin_addr.s_addr = htonl(INADDR_ANY);
-        dest_addr_ip4->sin_family = AF_INET;
-        dest_addr_ip4->sin_port = htons(TCP_C_PORT);
-        ip_protocol = IPPROTO_IP;
-    }
-
-    int listen_sock = socket(addr_family, SOCK_STREAM, ip_protocol);
-    if (listen_sock < 0) {
-        ESP_LOGE(TAG, "Unable to create socket: errno %d", errno);
-        vTaskDelete(NULL);
-        return;
-    }
-    int opt = 1;
-    setsockopt(listen_sock, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
-
-    ESP_LOGI(TAG, "Socket created");
-
-    err = bind(listen_sock, (struct sockaddr *)&dest_addr, sizeof(dest_addr));
-    if (err != 0) {
-        ESP_LOGE(TAG, "Socket unable to bind: errno %d", errno);
-        ESP_LOGE(TAG, "IPPROTO: %d", addr_family);
-    }
-    ESP_LOGI(TAG, "Socket bound, port %d", TCP_C_PORT);
-
-    err = listen(listen_sock, 1);
-    if (err != 0) {
+static int tcp_c_listen_on_socket(int socket) {
+    volatile err_c_t err = ERR_C_OK;
+    Try {
+        err = listen(socket, 1);
+        ERR_C_CHECK_AND_THROW_ERR(err);
+    } Catch(err) {
         ESP_LOGE(TAG, "Error occurred during listen: errno %d", errno);
     }
 
-    while (1) {
+    return err;
+}
 
-        ESP_LOGD(TAG, "Socket listening");
+static int tcp_c_accept_socket(int socket) {
+    volatile err_c_t err = ERR_C_OK;
+    volatile int sock;
+    char addr_str[128];
+    struct sockaddr_storage source_addr; // Large enough for both IPv4 or IPv6
+    socklen_t addr_len = sizeof(source_addr);
+    int keep_alive = TCP_C_KEEPALIVE;
+    int keep_idle = TCP_C_KEEPALIVE_IDLE;
+    int keep_interval = TCP_C_KEEPALIVE_INTERVAL;
+    int keep_count = TCP_C_KEEPALIVE_COUNT;
 
-        struct sockaddr_storage source_addr; // Large enough for both IPv4 or IPv6
-        socklen_t addr_len = sizeof(source_addr);
-        int sock = accept(listen_sock, (struct sockaddr *)&source_addr, &addr_len);
+    Try {
+        sock = accept(socket, (struct sockaddr *)&source_addr, &addr_len);
         if (sock < 0) {
-            ESP_LOGE(TAG, "Unable to accept connection: errno %d", errno);
-            break;
+            ERR_C_SET_AND_THROW_ERR(err, errno);
         }
 
         // Set tcp keepalive option
-        setsockopt(sock, SOL_SOCKET, SO_KEEPALIVE, &keepAlive, sizeof(int));
-        setsockopt(sock, IPPROTO_TCP, TCP_KEEPIDLE, &keepIdle, sizeof(int));
-        setsockopt(sock, IPPROTO_TCP, TCP_KEEPINTVL, &keepInterval, sizeof(int));
-        setsockopt(sock, IPPROTO_TCP, TCP_KEEPCNT, &keepCount, sizeof(int));
+        setsockopt(sock, SOL_SOCKET, SO_KEEPALIVE, &keep_alive, sizeof(int));
+        setsockopt(sock, IPPROTO_TCP, TCP_KEEPIDLE, &keep_idle, sizeof(int));
+        setsockopt(sock, IPPROTO_TCP, TCP_KEEPINTVL, &keep_interval, sizeof(int));
+        setsockopt(sock, IPPROTO_TCP, TCP_KEEPCNT, &keep_count, sizeof(int));
+        
         // Convert ip address to string
-
         if (source_addr.ss_family == PF_INET) {
             inet_ntoa_r(((struct sockaddr_in *)&source_addr)->sin_addr, addr_str, sizeof(addr_str) - 1);
         }
 
         ESP_LOGD(TAG, "Socket accepted ip address: %s", addr_str);
+    } Catch (err) {
+        ESP_LOGE(TAG, "Unable to accept connection: errno %d", err);
+    }
 
-        tcp_c_handle_exchange(sock);
+    return sock;
+}
 
-        shutdown(sock, 0);
-        close(sock);
+void tcp_c_server_loop(void)
+{
+    volatile err_c_t err = ERR_C_OK;
+    Try {
+        while (1) {
+
+            ESP_LOGD(TAG, "Socket listening");
+            //Accept new connection
+            connected_socket = tcp_c_accept_socket(listen_socket);
+            //Inform that new connection was accepted.
+            xEventGroupSetBits(tcp_event_group, TCP_C_ACCEPTED_SOCKET_BIT);
+            //Wait till transmission is finished.
+            xEventGroupWaitBits(tcp_event_group, TCP_C_FINISHED_TRANSMISSION, pdTRUE, pdFALSE, pdMS_TO_TICKS(3500));
+            //Close the connection.
+            shutdown(connected_socket, 0);
+            close(connected_socket);
+        }
+    } Catch(err) {
+        ESP_LOGE(TAG, "error: %d", err);
     }
 }
 
-void tcp_c_start_tcp_server(void) {
-    xTaskCreate(cli_tcp_server_task, "tcp_server", 4096, NULL, 5, NULL);
+int tcp_c_start_tcp_server(EventGroupHandle_t event_group) {
+    volatile err_c_t err = ERR_C_OK;
+    tcp_event_group = event_group;
+    struct sockaddr_storage dest_addr;
+    Try {
+        tcp_c_prepare_sock_addr(&dest_addr);
+        listen_socket = tcp_c_prepare_listen_socket();
+        ESP_LOGI(TAG, "Socket created");
+        ERR_C_CHECK_AND_THROW_ERR(tcp_c_bind_socket(listen_socket, &dest_addr));
+        ESP_LOGI(TAG, "Socket bound, port: %d", TCP_C_PORT);
+        ERR_C_CHECK_AND_THROW_ERR(tcp_c_listen_on_socket(listen_socket));
+    } Catch(err) {
+        ESP_LOGE(TAG, "Error during starting tcp server: %d", err);
+    }
+
+    return listen_socket;
 }
